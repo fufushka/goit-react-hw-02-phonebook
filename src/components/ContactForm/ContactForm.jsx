277b22c758a80bb0c4406dbd49ecf81019f3f987
fufushka <dimaukropdred@gmail.com>
2023-07-16 14:37:46 +0300

    ф

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -32,7 +32,7 @@ export default class ContactForm extends Component {
             placeholder="Name"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
             className={css.form__input}
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            // pattern="^[a\-zA\-Zа\-яА-Я]+((['\-][a\-zA\-Zа\-яА\-Я ])?[a\-zA\-Zа\-яА\-Я]*)*$"
             id="name"
             name="name"
             value={this.state.name}
@@ -50,7 +50,7 @@ export default class ContactForm extends Component {
             className={css.form__input}
             id="email"
             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            pattern="^\+?\d{1,4}[-.\s]?\(?\d{1,3}\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$"
+            // pattern="^\+?\d{1,4}[#\-.\s]?\(?\d{1,3}\)?[\-.\s]?\d{1,4}[\-.\s]?\d{1,4}[\-.\s]?\d{1,9}$"
             value={this.state.number}
             onChange={this.handleChange}
           />
